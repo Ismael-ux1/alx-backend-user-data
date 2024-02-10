7d15e0a0802554524ac1a11af44a328d44ff56d8
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-10 21:15:06 +0000

    updated Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,13 +5,7 @@ import re
 
 def filter_datum(fields: list, redaction: str, message: str,
                  separator: str) -> str:
-    """ A function that returns the log message obfuscated """
-
-    # Create a regex pattern for each field to be obfuscated
     pattern = '|'.join(f'{field}=[^;]*' for field in fields)
-
-    # Replace all occurrences of the fields in the message,
-    # with the redaction string
     return re.sub(pattern,
                   lambda m: m.group().replace(
                       m.group().split('=')[1], redaction),
