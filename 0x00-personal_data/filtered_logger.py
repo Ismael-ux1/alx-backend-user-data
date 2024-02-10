cb896a2680aea27b7cd4d271b9e1f2b5f61aee7d
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-10 21:36:55 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: [list], redaction: str, message: str,
                  separator: str) -> str:
     """ A function that returns the log message obfuscated """
     for field in fields:
