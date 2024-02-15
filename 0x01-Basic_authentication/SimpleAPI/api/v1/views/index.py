3922d638f89e60c68ad31ec30ca1ab3bbf5e38a0
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-15 20:48:17 +0000

    updated Simple-basic-API

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Blueprint
 from api.v1.views import app_views
 
 
@@ -17,10 +17,14 @@ def status() -> str:
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
-    Return:
+    Return
       - the number of each objects
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route():
+    abort(401)
