216d3a5700956f8bd1114564beab7d93fc2b17de
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-16 17:47:45 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,7 +13,6 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -24,3 +23,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """GET /api/v1/unauthorized"""
+    abort(401)
