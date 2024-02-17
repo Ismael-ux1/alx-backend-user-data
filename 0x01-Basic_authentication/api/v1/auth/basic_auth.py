a24b684ce053570b929f5bba232fae66e2ff0dc0
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-17 01:21:31 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Basic auth """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -29,3 +30,31 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Method that returns the decoded value of,
+        a Base64 string base64_authorization_header.
+
+        Parameters:
+        base64_authorization_header (str):
+        The Base64 part of the Authorization header.
+
+        Returns:
+        str: The decoded value of base64_authorization_header,
+        if it is a valid Base64 string, None otherwise.
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64.b64decode(base64_bytes)
+            return message_bytes.decode('utf-8')
+        except Exception:
+            return None
