8a625191b30570c6ebaa8c121452178c10631e50
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-17 00:14:55 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,12 +6,49 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+if os.getenv('AUTH_TYPE') == 'auth':
+    auth = Auth()
+
+
+@app.before_request
+def before_request_func():
+    """
+    checks if the request path requires authentication and,
+    aborts the request with the appropriate status code if necessary.
+    """
+
+    # If auth is None, do nothing and proceed with the request
+    if auth is None:
+        return
+
+    # List of paths that do not require authentication
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    # If the request path does not require authentication,
+    # do nothing and proceed with the request
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    # If the request does not have an Authorization header,
+    # abort the request with a 401 Unauthorized status
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    # If there is no current user,
+    # abort the request with a 403 Forbidden status
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
