dfaa4f687b790c0d7ed1e1bcd12e295acf362760
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-18 19:50:36 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,10 +63,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
 
     # Retrieve database credentials from environment variables
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
     return mysql.connector.connect(
         user=username, password=password, host=host, database=db_name
