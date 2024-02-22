24551cc78f3d38fe3fd85b5f46b08767b2696c1e
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-22 23:40:47 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -111,7 +111,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'])
-def update_password():
+def update_password() -> str:
     """ function that respond to the PUT /reset_password route."""
     # Get the email, reset token, and new password from the form data
     email = request.form.get('email')
