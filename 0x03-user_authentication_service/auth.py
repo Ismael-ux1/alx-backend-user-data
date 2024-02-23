e660d1448c9c925d434a24fd2fd6761103f65132
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-23 02:35:12 +0000

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -194,9 +194,6 @@ class Auth:
             # If the user does not exist, raise a ValueError
             raise ValueError()
 
-        # If the user exists, hash the new password
-        hashed_password = self._hash_password(password)
-
         # Update the user's hashed_password field with the new hashed password
         # and the reset_token field to None
         self._db.update_user(user.id,
